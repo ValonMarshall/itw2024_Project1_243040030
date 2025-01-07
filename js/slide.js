eb695c682d4a3d86e8f58c9b2982ec45ede456a0
ValonMarshall <dionavalon32238@gmail.com>
2025-01-07 08:22:32 +0700

    finalisasi

---
+++ js/slide.js
@@ -0,0 +1,56 @@
+// Navigation
+const menuButton = document.querySelector('.menu-btn .home-btn')
+const navbar = document.querySelector('.navbar')
+
+menuButton.addEventListener('click', function() {
+    navbar.classList.toggle('slide');
+});
+
+// In skills-------
+
+const tabButton = document.querySelector('.tabs-element .html')
+const infoskills = document.querySelector('.info-skills ')
+
+tabButton.addEventListener('click', function() {
+    infoskills.classList.toggle('slide');
+});
+
+// 
+const tabButton2 = document.querySelector('.tabs-element .css')
+const infoskills2 = document.querySelector('.info-skills2 ')
+
+tabButton2.addEventListener('click', function() {
+    infoskills2.classList.toggle('slide');
+});
+
+// 
+const tabButton3 = document.querySelector('.tabs-element .cplus')
+const infoskills3 = document.querySelector('.info-skills3 ')
+
+tabButton3.addEventListener('click', function() {
+    infoskills3.classList.toggle('slide');
+});
+
+// 
+const tabButton4 = document.querySelector('.tabs-element .python')
+const infoskills4 = document.querySelector('.info-skills4 ')
+
+tabButton4.addEventListener('click', function() {
+    infoskills4.classList.toggle('slide');
+});
+
+// 
+const tabButton5 = document.querySelector('.tabs-element .java')
+const infoskills5 = document.querySelector('.info-skills5 ')
+
+tabButton5.addEventListener('click', function() {
+    infoskills5.classList.toggle('slide');
+});
+
+// 
+const tabButton6 = document.querySelector('.tabs-element .js')
+const infoskills6 = document.querySelector('.info-skills6 ')
+
+tabButton6.addEventListener('click', function() {
+    infoskills6.classList.toggle('slide');
+});
